8daa0025e628815dbe95833682bcb4f4e05c7f60
micap96 <39765771+micap96@users.noreply.github.com>
2020-12-27 17:10:18 +0100

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var gls = require('gulp-live-server');
+
+gulp.task('serve', function() {
+    var server = gls.static('static'); 
+    server.start();
+
+    gulp.watch(['static/**/*.css', 'static/**/*.html', 'static/**/*.js'], function (file) {
+        server.notify.apply(server, [file]);
+        
+        var timestamp = new Date().getHours() + ":" + new Date().getMinutes() + ":" +  new Date().getSeconds();
+        console.debug(timestamp + " change detected");
+    });
+});
